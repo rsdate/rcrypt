b517dbfdbac6f1ed81cc289bc8aa1b583929bb80
Rohan S. Date <rohan.s.date@icloud.com>
2026-01-31 21:38:51 -0800

    Update __init__.py

--- rqcrypt/__init__.py
+++ rqcrypt/__init__.py
@@ -1,2 +1,2 @@
-from ShadowCrypt.examples.example_scrngV2 import example_scrngV2  # noqa: F401
-from ShadowCrypt.scrngV2.scrngV2 import random_nums  # noqa: F401
+from rqcrypt.examples.example_scrngV2 import example_scrngV2  # noqa: F401
+from rqcrypt.scrngV2.scrngV2 import random_nums  # noqa: F401
